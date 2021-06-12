df6c040c767ec9d02569531e5923fef95c11f222
Paul Tuck <pdrt888@gmail.com>
2021-06-13 05:17:57 +0800

    "2021-06-13T05:17CST"

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,13 +1,40 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
+#[macro_use] extern crate rocket;
+
+use rocket::fairing::AdHoc;
+use rocket::Rocket;
+use rocket::{get, routes};
+use rocket_contrib::serve::StaticFiles;
+
+fn static_files(rocket: Rocket) -> Result<Rocket, Rocket> {
+    const DEFAULT_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/static");
+
+    let dir = rocket
+        .config()
+        .get_str("static_dir")
+        .unwrap_or(DEFAULT_DIR)
+        .to_string();
+
+    let static_files = StaticFiles::from(dir);
+
+    let rocket = rocket.mount("/", static_files);
+
+    Ok(rocket)
+}
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, World!"
+    "Hello rocket!"
+}
+
+fn ignite() -> rocket::Rocket {
+    rocket::ignite()
+        .attach(AdHoc::on_attach("Static files", static_files))
+        .mount("/", routes![index])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    env_logger::init();
+    ignite().launch();
 }
